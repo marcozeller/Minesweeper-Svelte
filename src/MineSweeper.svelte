e52ccb25c19aa4e22f03e98339db8e1aeb2430ce
mzeller@ethz.ch <marcozeller@bluewin.ch>
2023-07-14 14:09:03 +0200

    implemented basic functionality

--- src/MineSweeper.svelte
+++ src/MineSweeper.svelte
@@ -1,78 +1,169 @@
 <script>
-    import MineSweeperCell from "./MineSweeperCell.svelte";
+  import MineSweeperCell from "./MineSweeperCell.svelte";
 
-    export let field_size = 8;
-    export let n_mines = 10;
-    let field = Array(field_size + 2)
-        .fill(0)
-        .map((x) => Array(field_size + 2).fill(0));
+  export let field_size = 8;
+  export let n_mines = 10;
+  let field = Array(field_size + 2)
+    .fill(0)
+    .map((x) => Array(field_size + 2).fill(0));
 
-    console.log(field);
+  for (let x = 0; x < field_size + 2; x++) {
+    for (let y = 0; y < field_size + 2; y++) {
+      field[x][y] = { content: 0 };
+    }
+  }
 
-    for (let i = 0; i < n_mines; i++) {
-        let x = Math.floor(Math.random() * field_size + 1);
-        let y = Math.floor(Math.random() * field_size + 1);
-        while (field[x][y] == "mine") {
-            x = Math.floor(Math.random() * field_size + 1);
-            y = Math.floor(Math.random() * field_size + 1);
+  console.log(field);
+
+  for (let i = 0; i < n_mines; i++) {
+    let x = Math.floor(Math.random() * field_size + 1);
+    let y = Math.floor(Math.random() * field_size + 1);
+    while (field[x][y].content == "mine") {
+      x = Math.floor(Math.random() * field_size + 1);
+      y = Math.floor(Math.random() * field_size + 1);
+    }
+    field[x][y].content = "mine";
+  }
+
+  for (let x = 1; x < field_size + 1; x++) {
+    for (let y = 1; y < field_size + 1; y++) {
+      field[x][y].x = x - 1;
+      field[x][y].y = y - 1;
+      field[x][y].state = "hidden";
+      if (field[x][y].content == "mine") {
+        continue;
+      }
+      let count = 0;
+      if (field[x - 1][y - 1].content == "mine") {
+        count++;
+      }
+      if (field[x - 1][y].content == "mine") {
+        count++;
+      }
+      if (field[x - 1][y + 1].content == "mine") {
+        count++;
+      }
+      if (field[x][y - 1].content == "mine") {
+        count++;
+      }
+      if (field[x][y + 1].content == "mine") {
+        count++;
+      }
+      if (field[x + 1][y - 1].content == "mine") {
+        count++;
+      }
+      if (field[x + 1][y].content == "mine") {
+        count++;
+      }
+      if (field[x + 1][y + 1].content == "mine") {
+        count++;
+      }
+      field[x][y].content = count;
+    }
+  }
+  field = field.slice(1, -1);
+  field = field.map((row) => row.slice(1, -1));
+
+  const loose_game = (event) => {
+    alert("You loose!");
+  };
+
+  const uncover_neighbours = (x, y) => {
+    field[x][y].state = "open";
+    if (x > 0) {
+      if (y > 0) {
+        if (
+          field[x - 1][y - 1].content == 0 &&
+          field[x - 1][y - 1].state != "open"
+        ) {
+          uncover_neighbours(x - 1, y - 1);
         }
-        field[x][y] = "mine";
+        field[x - 1][y - 1].state = "open";
+      }
+      if (field[x - 1][y].content == 0 && field[x - 1][y].state != "open") {
+        uncover_neighbours(x - 1, y);
+      }
+      field[x - 1][y].state = "open";
+
+      if (y < field_size - 1) {
+        if (
+          field[x - 1][y + 1].content == 0 &&
+          field[x - 1][y + 1].state != "open"
+        ) {
+          uncover_neighbours(x - 1, y + 1);
+        }
+        field[x - 1][y + 1].state = "open";
+      }
     }
 
-    for (let x = 1; x < field_size + 1; x++) {
-        for (let y = 1; y < field_size + 1; y++) {
-            if (field[x][y] == "mine") {
-                continue;
-            }
-            let count = 0;
-            if (field[x - 1][y - 1] == "mine") {
-                count++;
-            }
-            if (field[x - 1][y] == "mine") {
-                count++;
-            }
-            if (field[x - 1][y + 1] == "mine") {
-                count++;
-            }
-            if (field[x][y - 1] == "mine") {
-                count++;
-            }
-            if (field[x][y + 1] == "mine") {
-                count++;
-            }
-            if (field[x + 1][y - 1] == "mine") {
-                count++;
-            }
-            if (field[x + 1][y] == "mine") {
-                count++;
-            }
-            if (field[x + 1][y + 1] == "mine") {
-                count++;
-            }
-            field[x][y] = count;
-        }
+    if (y > 0) {
+      if (field[x][y - 1].content == 0 && field[x][y - 1].state != "open") {
+        uncover_neighbours(x, y - 1);
+      }
+      field[x][y - 1].state = "open";
     }
-    field = field.slice(1, -1);
-    field = field.map((row) => row.slice(1, -1));
+    if (y + 1 < field_size) {
+      if (field[x][y + 1].content == 0 && field[x][y + 1].state != "open") {
+        uncover_neighbours(x, y + 1);
+      }
+      field[x][y + 1].state = "open";
+    }
+
+    if (x + 1 < field_size) {
+      if (y > 0) {
+        if (
+          field[x + 1][y - 1].content == 0 &&
+          field[x + 1][y - 1].state != "open"
+        ) {
+          uncover_neighbours(x + 1, y - 1);
+        }
+        field[x + 1][y - 1].state = "open";
+      }
+      if (field[x + 1][y].content == 0 && field[x + 1][y].state != "open") {
+        uncover_neighbours(x + 1, y);
+      }
+      field[x + 1][y].state = "open";
+      if (y + 1 < field_size) {
+        if (
+          field[x + 1][y + 1].content == 0 &&
+          field[x + 1][y + 1].state != "open"
+        ) {
+          uncover_neighbours(x + 1, y + 1);
+        }
+        field[x + 1][y + 1].state = "open";
+      }
+    }
+  };
+
+  const zero_uncovered = (event) => {
+    uncover_neighbours(event.detail.x, event.detail.y);
+  };
 </script>
 
 <div class="field">
-    {#each field as row}
-        <div class="row">
-            {#each row as cell}
-                <MineSweeperCell content={cell} />
-            {/each}
-        </div>
-    {/each}
+  {#each field as row}
+    <div class="row">
+      {#each row as cell}
+        <MineSweeperCell
+          content={cell.content}
+          x={cell.x}
+          y={cell.y}
+          bind:state={cell.state}
+          on:loose_game={loose_game}
+          on:zero_uncovered={zero_uncovered}
+        />
+      {/each}
+    </div>
+  {/each}
 </div>
 
 <style>
-    .field {
-        display: flex;
-        flex-direction: column;
-    }
-    .row {
-        display: flex;
-        flex-direction: row;
-    }
+  .field {
+    display: flex;
+    flex-direction: column;
+  }
+  .row {
+    display: flex;
+    flex-direction: row;
+  }
 </style>
--- src/MineSweeperCell.svelte
+++ src/MineSweeperCell.svelte
@@ -1,28 +1,61 @@
 <script>
-    export let content;
-    let state = "hidden";
-    const clicked_on = () => {
-        state = "open";
-    };
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+
+  export let content;
+  export let x;
+  export let y;
+  export let state = "hidden";
+
+  const clicked_on = () => {
+    state = "open";
+    if (content == "mine") {
+      dispatch("loose_game", {});
+    }
+    if (content == 0) {
+      dispatch("zero_uncovered", { x: x, y: y });
+    }
+  };
 </script>
 
 {#if state == "hidden"}
-    <div class="cell hidden" on:click={clicked_on} />
+  <div class="cell hidden center" on:click={clicked_on} />
 {:else if state == "flagged"}
-    <div class="cell flagged" on:click={clicked_on}>?</div>
+  <div class="cell flagged center" on:click={clicked_on}>
+    <p>?</p>
+  </div>
 {:else}
-    <div class="cell open">{content}</div>
+  <div class="cell open center">
+    {#if content == "mine"}
+      <p>boom!</p>
+    {:else}
+      <p>{content}</p>
+    {/if}
+  </div>
 {/if}
 
 <style>
-    .cell {
-        height: 80px;
-        width: 80px;
-    }
-    .flagged {
-    }
-    .hidden {
-        background-image: url("http://i47.tinypic.com/2ni0ahd.png");
-        background-size: cover;
-    }
+  .cell {
+    height: 80px;
+    width: 80px;
+    border: solid grey 1px;
+    margin: 2px;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    position: relative;
+    text-align: center;
+    vertical-align: middle;
+  }
+  .flagged {
+  }
+  .hidden {
+    background-color: lightgrey;
+    background-size: cover;
+  }
+  .center p {
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
 </style>
